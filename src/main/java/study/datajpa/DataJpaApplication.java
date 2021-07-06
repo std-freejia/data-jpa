2117ee8d14fc486e3af603fcc4dc408dd6b86a5a
std-freejia <std-freejia@gmail.com>
2021-07-07 01:55:45 +0900

    공통 인터페이스 설정
    @EnableJpaRepositories(basePackages = "study.datajpa.repository")
    // springboot가 자동 처리해준다.
    
    개발자가 인터페이스를 만들면, 스프링 데이터 jpa가 구현클래스를 생성해준다.

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,8 +2,10 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+// @EnableJpaRepositories(basePackages = "study.datajpa.repository") // springboot가 자동 처리해준다.
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
